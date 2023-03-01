eca21105bc31611f1ac6da9e11a0bcd803106975
fernando zuniga <carlosfernando.zh@gmail.com>
2023-02-28 20:18:59 -0600

    react practice

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,12 @@
-function ImageList() {
-  return <div>ImageList</div>
+import './ImageList.css';
+import  ImageShow from './ImageShow';
+
+function ImageList({ images }) {
+  const renderImages = images.map((image) => {
+    return <ImageShow key={image.id} image={image}/>
+  })
+
+  return <div className='image-list' >{renderImages}</div>
 }
 
 export default ImageList;
