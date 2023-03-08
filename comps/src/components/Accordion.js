e77a381e5c31e5410fd6b31b9241c7ca94c94995
fernando zuniga <carlosfernando.zh@gmail.com>
2023-03-07 21:26:07 -0600

    expand collaps component practice

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,15 +1,30 @@
-function Accordion({ items }) {
+import { useState } from "react";
+import { BiCaretLeft, BiCaretDown } from "react-icons/bi";
 
-  const renderedItems = items.map((item) => {
+function Accordion({ items }) {
+  const [ expandedIndex, setExpandedItem] = useState(-1);
+
+  const handleClick = (index) => {
+    index === expandedIndex ? setExpandedItem(-1) : setExpandedItem(index);
+    }
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = <span className="text-xl ">{isExpanded ? <BiCaretDown /> : <BiCaretLeft /> }</span>
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+        onClick={() => {handleClick(index)}} >{item.label}
+        {icon}
+        </div>
+        { isExpanded && <div className="border-b p-5" >{item.content}</div> }
       </div>
     )
   });
 
-  return <div>{renderedItems}</div>
+  return <div className="border-x border-t rounded" >{renderedItems}</div>
 };
 
 export default Accordion;
