349dc7316c2cdd55ae0959bdd33d8a2e1e8c42d5
fernando zuniga <carlosfernando.zh@gmail.com>
2023-03-10 19:30:38 -0600

    dropdown component work

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { TiArrowSortedDown } from "react-icons/ti";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false)
+
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if(!divEl.current){
+        return;
+      }
+      if(!divEl.current.contains(event.target)){
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return document.removeEventListener('click', handler);
+  }, []);
+
 
   const clickHandler = () => setIsOpen(!isOpen);
 
@@ -19,7 +37,7 @@ function Dropdown({ options, value, onChange }) {
 
 
   return (
-  <div className="w-48 relative">
+  <div ref={divEl} className="w-48 relative">
   <Panel className="flex justify-between items-center cursor-pointer"
    onClick={clickHandler}>{value?.label || 'Select...' }
    <TiArrowSortedDown className="text-lg"/>
